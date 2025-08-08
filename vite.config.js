44e01271100049bac02a0c90049e0aaf9b8c965e
Ayush Verma <vermaayush680@gmail.com>
2025-08-08 15:46:25 +0530

    fixed URL

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://naas.isalman.dev/no',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
