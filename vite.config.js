cbef33fcda74ca4a3cd1960faa800adf8383a442
Ayush Verma <vermaayush680@gmail.com>
2025-08-08 15:32:56 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
-dotenv.config()
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,
+        target: 'https://naas.isalman.dev/no',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
